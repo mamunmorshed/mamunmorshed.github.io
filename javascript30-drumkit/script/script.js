1038e5d1654ef53098a0dbac35468120ce4db6d7
Mamun Morshed <imroony@gmail.com>
2017-02-17 16:29:10 +0600

    Added javascript30-drumkit practice files

--- javascript30-drumkit/script/script.js
+++ javascript30-drumkit/script/script.js
@@ -1,3 +1,21 @@
 /**
  * Created by Mamun Morshed on 2/14/2017.
  */
+
+function playSound(e){
+    const audio = document.querySelector("audio[data-key=\""+e.keyCode+"\"]");
+    if (!audio) return;
+    const key = document.querySelector(".key[data-key=\""+e.keyCode+"\"]");
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+function dimKeyLight(e){
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll(".key");
+for( let i = 0; i < keys.length; i++ ){keys[i].addEventListener('transitionend', dimKeyLight);}
+
+window.addEventListener('keydown', playSound);
